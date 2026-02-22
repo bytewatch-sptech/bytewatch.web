f7f0d126400705bb673dcb73f2796d8003f0f5d3
Gustavo Pietro <gusoares.ma@gmail.com>
2026-02-22 11:48:18 -0300

    Adicionando Transição de cores na navbar e ajustando a navbar-collapse Mobile

--- public/index.js
+++ public/index.js
@@ -7,9 +7,16 @@ window.addEventListener('scroll', function() {
   // Se rolar mais de 50px, adiciona a classe 'scrolled' e muda a versão logo, senão remove ou muda
   if (window.scrollY > 50) {
     navbar.classList.add('scrolled');
+    navbar.classList.remove('navbar-dark');
+    navbar.classList.add('navbar-light');
+
     logo.src = "assets/logo-versao1.png";
   } else {
     navbar.classList.remove('scrolled');
+     navbar.classList.remove('navbar-light');
+    navbar.classList.add('navbar-dark');
+
     logo.src = "assets/logo-versao2.png";
   }
 });
+
