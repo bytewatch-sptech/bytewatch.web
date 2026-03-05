851590079ca30cb681e42560760d08310099cfe2
Jortieke <joao.jortiekejr@sptech.school>
2026-03-05 14:09:14 -0300

    Atualização de Script de BD

--- Site Institucional/src/database/script-tabelas.sql
+++ Site Institucional/src/database/script-tabelas.sql
@@ -8,6 +8,14 @@ CREATE TABLE tipo_usuario (
     tipo VARCHAR(45) NOT NULL
 );
 
+CREATE TABLE empresa(
+	id_empresa INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(45) NOT NULL,
+    cnpj CHAR(13) UNIQUE NOT NULL,
+    telefone CHAR(11) UNIQUE NOT NULL,
+    email VARCHAR(45) UNIQUE NOT NULL    
+);
+
 CREATE TABLE usuario (
     id_usuario INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -17,8 +25,13 @@ CREATE TABLE usuario (
     fk_tipo_usuario INT NOT NULL,
     CONSTRAINT fk_usuario_tipo
         FOREIGN KEY (fk_tipo_usuario)
-        REFERENCES tipo_usuario(id_tipo_usuario)
+			REFERENCES tipo_usuario(id_tipo_usuario),
+	fk_usuario_empresa INT NOT NULL,
+    CONSTRAINT fk_empresa
+		FOREIGN KEY (fk_usuario_empresa)
+			REFERENCES empresa(id_empresa)
 );
+
 
 -- MONITORAMENTO
 
@@ -28,7 +41,10 @@ CREATE TABLE servidor (
     endereco_ip VARCHAR(45) NOT NULL,
     sistema_operacional VARCHAR(45) NOT NULL,
     localizacao VARCHAR(100),
-    ambiente ENUM('producao', 'homologacao', 'dev') NOT NULL
+    ambiente ENUM('producao', 'homologacao', 'dev') NOT NULL,
+    fk_servidor_empresa INT NOT NULL,
+		FOREIGN KEY (fk_servidor_empresa)
+			REFERENCES empresa(id_empresa)
 );
 
 CREATE TABLE componente (
@@ -39,7 +55,24 @@ CREATE TABLE componente (
     descricao VARCHAR(200),
     CONSTRAINT fk_componente_servidor
         FOREIGN KEY (fk_servidor)
-        REFERENCES servidor(id_servidor)
+			REFERENCES servidor(id_servidor)
+);
+
+CREATE TABLE componente_servidor(
+	fk_servidor INT,
+    fk_empresa_componente INT,
+    fk_componente_servidores INT,
+		CONSTRAINT pk_componente_servidor
+			PRIMARY KEY (fk_servidor, fk_empresa_componente, fk_componente_servidores),
+		CONSTRAINT fk_servidor
+			FOREIGN KEY (fk_servidor)
+				REFERENCES servidor(id_servidor),
+		CONSTRAINT fk_empresa_componente
+			FOREIGN KEY (fk_empresa_componente)
+				REFERENCES empresa(id_empresa),
+		CONSTRAINT fk_componente_servidores
+			FOREIGN KEY (fk_componente_servidores)
+				REFERENCES componente(id_componente)
 );
 
 CREATE TABLE registro (
@@ -68,3 +101,7 @@ CREATE TABLE alerta (
 
 insert into tipo_usuario (tipo) values ('adm');
 insert into tipo_usuario (tipo) values ('user');
+
+SELECT id_usuario, nome, email, fk_tipo_usuario FROM usuario WHERE email = 'joao.jortiekejr@sptech.school';
+
+SELECT * FROM usuario;
