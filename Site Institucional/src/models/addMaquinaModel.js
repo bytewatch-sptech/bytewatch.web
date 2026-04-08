53a24b0e1de0ae6ab1150bba769b606796c19882
Rian04CCO <rian.silva@sptech.school>
2026-04-08 20:49:18 -0300

    até o momento finalmente tudo funciona

--- Site Institucional/src/models/addMaquinaModel.js
+++ Site Institucional/src/models/addMaquinaModel.js
@@ -12,11 +12,11 @@ function cadastrar(nome, localizacao, ip, fkEmpresa, componentes) {
       var compo = componentes[i];
 
       if (compo.qtd > 0) {
-        var sqlComp = `INSERT INTO componente_servidor (fk_id_servidor, fk_id_empresa, fk_id_componente, limite) 
-                               VALUES ('${idServidor}', '${idEmpresa}', '${compo.idComponente}', '${compo.qtd}')`
+        var sqlComp = `INSERT INTO componente_servidor (fk_servidor, fk_empresa_componente, fk_componente_servidores) 
+                               VALUES ('${idServidor}', '${fkEmpresa}', '${compo.idComponente}');`
 
         var sqlAlerta = `INSERT INTO parametro_alerta (nivel, valor_limite, fk_id_servidor, fk_id_empresa, fk_id_componente) 
-                               VALUES ('Crítico', ${comp.limiteAlerta}, ${idServidor}, ${idEmpresa}, ${comp.idComponente})`;
+                               VALUES ('Crítico', '${compo.limiteAlerta}', '${idServidor}', '${fkEmpresa}', '${compo.idComponente}');`;
 
         promessas.push(database.executar(sqlComp));
         promessas.push(database.executar(sqlAlerta));
