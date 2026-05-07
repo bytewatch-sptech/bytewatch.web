98ec26170e65415f7cc193b560c91722a401ca01
lluan-iada <luan.nascimento@sptech.school>
2026-05-06 22:19:44 -0300

    router - rota da função deletar analista

--- Site Institucional/src/routes/usuarios.js
+++ Site Institucional/src/routes/usuarios.js
@@ -12,4 +12,8 @@ router.post("/autenticar", function (req, res) {
     usuarioController.autenticar(req, res);
 });
 
+router.delete("/deletar/:idUsuario", function (req, res) {
+    usuarioController.deletar(req, res);
+});
+
 module.exports = router;
