633b7ef71ac1fb49bc6bd3d4048e1abbba8c969f
lluan-iada <luan.nascimento@sptech.school>
2026-05-07 21:00:28 -0300

    Controller - função listar

--- Site Institucional/src/controllers/usuarioController.js
+++ Site Institucional/src/controllers/usuarioController.js
@@ -101,8 +101,23 @@ function cadastrar(req, res) {
         );
 }
 
+function listar(req, res) {
+    usuarioModel.listar()
+        .then(function (resultado) {
+            if (resultado.length > 0) {
+                res.status(200).json(resultado);
+            } else {
+                res.status(204).send("Nenhum resultado encontrado!");
+            }
+        }).catch(function (erro) {
+            console.log(erro);
+            res.status(500).json(erro.sqlMessage);
+        });
+}
+
 module.exports = {
     autenticar,
     cadastrar,
-    deletar
+    deletar,
+    listar
 }
