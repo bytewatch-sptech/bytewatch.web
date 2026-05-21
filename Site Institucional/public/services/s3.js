c53b00e85a6696ae721e8390941d940adcf913ac
VinnyMendess <vinicius.msilva@sptech.school>
2026-05-20 23:14:09 -0300

    configuracao aws

--- Site Institucional/public/services/s3.js
+++ Site Institucional/public/services/s3.js
@@ -10,37 +10,21 @@ const streamToString = (stream) =>
 
     })
 
-async function obterUsoServidor(macAddress) {
+async function obterDadosS3() {
 
     const params = {
         Bucket: process.env.S3_BUCKET_NAME,
         Key: "client/client.json"
     }
 
-    try{
+    try {
         const { Body } = await s3Client.send(new GetObjectCommand(params))
         const conteudo = await streamToString(Body)
-        const bancoDadosJson = JSON.parse(conteudo)
-
-        const dadosMaquina = bancoDadosJson[macAddress]
-        if(!dadosMaquina)return null;
-        
-        const cpu = dadosMaquina.metricas.find(m => m.tipoDado === "cpu")?.porcentagemCpu || 0;
-        const ram = dadosMaquina.metricas.find(m => m.tipoDado === "ram")?.porcentagemRam || 0;
-        const disco = dadosMaquina.metricas.find(m => m.tipoDado === "disco")?.porcentagemDisco || 0;
-
-        return{
-            macAddress,
-            usoAtual: {
-                cpu: Math.round(cpu),
-                ram: Math.round(ram),
-                disco: Math.round(disco)
-            }
-        }
-    }catch(err){
-        console.error("Erro ao processar componentes:", err)
+        return JSON.parse(conteudo); // Retorna o JSON todinho
+    } catch(err) {
+        console.error("Erro ao puxar dados do S3:", err)
         throw err;
     }
 }
 
-module.exports = { obterUsoServidor }
+module.exports = { obterDadosS3 }
