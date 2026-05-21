c6c4ba393ccd72d42161cfa523b2ed3e2de923e0
lluan-iada <luan.nascimento@sptech.school>
2026-05-21 19:47:37 -0300

    Ajuste no Controller

--- Site Institucional/src/controllers/jiraController.js
+++ Site Institucional/src/controllers/jiraController.js
@@ -3,17 +3,18 @@ const jiraService = require("../../public/services/jiraService");
 async function filtrarDashboard(req, res) {
     try {
         var fkEmpresa = req.params.fkEmpresa;
-        // var fkFuncionario = req.params.fkFuncionario;
+        var fkFuncionario = req.params.fkFuncionario;
 
         const dados = await jiraService.filtrarDashboard(fkEmpresa);
 
         res.status(200).json(dados);
 
     } catch (erro) {
-        console.error("Erro no Jira Controller: ", erro);
+
         res.status(500).json({
             erro: "Erro ao buscar issues"
         });
+
     }
 }
 
