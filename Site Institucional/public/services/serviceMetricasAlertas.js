78ce499ab1d3f372fee5362df5506917931773f8
lluan-iada <luan.nascimento@sptech.school>
2026-05-21 09:46:34 -0300

    Conexão da rota do nome do servidor

--- Site Institucional/public/services/serviceMetricasAlertas.js
+++ Site Institucional/public/services/serviceMetricasAlertas.js
@@ -5,3 +5,11 @@ async function MetricasAlertas(){
     console.log(json);
     return json
 } 
+
+async function ObterNomeServidor(macAddress){
+    const body = {}
+    const response = await api.get(`/servidor/buscar-nome-servidor/${macAddress}`, body)
+    const json = await response.json()
+    console.log(json);
+    return json
+} 
