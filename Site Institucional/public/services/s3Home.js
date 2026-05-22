4eec10fc3987cb8dd8c640126d66448546efda5f
luisfelip2005 <gomesluisfelipe09@gmail.com>
2026-05-21 21:07:37 -0300

    resolvendo erros no merge

--- Site Institucional/public/services/s3Home.js
+++ Site Institucional/public/services/s3Home.js
@@ -27,4 +27,39 @@ async function obterDadosS3() {
     }
 }
 
-module.exports = { obterDadosS3 }
+
+async function obterUsoServidor(macAddress) {
+
+    const params = {
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: "client/client.json"
+    }
+
+    try{
+        const { Body } = await s3Client.send(new GetObjectCommand(params))
+        const conteudo = await streamToString(Body)
+        const bancoDadosJson = JSON.parse(conteudo)
+
+        const dadosMaquina = bancoDadosJson[macAddress]
+        if(!dadosMaquina)return null;
+        
+        const cpu = dadosMaquina.metricas.find(m => m.tipoDado === "cpu")?.porcentagemCpu || 0;
+        const ram = dadosMaquina.metricas.find(m => m.tipoDado === "ram")?.porcentagemRam || 0;
+        const disco = dadosMaquina.metricas.find(m => m.tipoDado === "disco")?.porcentagemDisco || 0;
+
+        return{
+            macAddress,
+            usoAtual: {
+                cpu: Math.round(cpu),
+                ram: Math.round(ram),
+                disco: Math.round(disco)
+            }
+        }
+    }catch(err){
+        console.error("Erro ao processar componentes:", err)
+        throw err;
+    }
+}
+
+
+module.exports = { obterDadosS3, obterUsoServidor }
